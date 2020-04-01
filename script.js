2ba312e35e8a10a215faa3866e0c47fc9938590b
Adam DeLessio <adamdelessio@gmail.com>
2020-04-01 19:10:13 -0400

    getting started

---
+++ script.js
@@ -0,0 +1,15 @@
+const field = document.querySelector('.field')
+const addPerformer = document.querySelector('.add-dot')
+
+addPerformer.addEventListener('click', changeColor)
+
+function changeColor() {
+	field.style.backgroundColor = 'red'
+}
+
+
+
+function newShow() {
+
+}
+
